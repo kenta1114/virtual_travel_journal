7808e75971fef2e8d566d0c9275cbdb2b1d84e16
kenta <user@MacBook-Air.local>
2026-05-26 14:36:24 +0900

    fix: correct vercel.json routing for PUT /api/travel/:id

--- api/travel/[id].js
+++ api/travel/[id].js
@@ -4,10 +4,7 @@ export default async function handler(req, res) {
   // CORS設定
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,PUT",
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,PUT");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
